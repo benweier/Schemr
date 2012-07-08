2df2d670e47f9c82e88f542047f6465f0fc034a1
Ben Weier <ben.weier@gmail.com>
2012-07-09 00:10:08 +1000

    Initial release

---
+++ readme.md
@@ -0,0 +1,9 @@
+# About
+Schemr allows your to select your color scheme from the command palatte.
+
+# Installation
+Install Schemr through [Package Control](http://wbond.net/sublime_packages/package_control) or download and extract it into your Packages folder.
+
+# Usage
+Type "Schemr" or the name of a color scheme into the command pallete and select it.
+Use the "Schemr: Reload schemes" command after installing or removing a color scheme to update the command palette options.
---
+++ schemr.py
@@ -0,0 +1,41 @@
+import sublime, sublime_plugin
+import os
+import json
+import sys
+
+settings = sublime.load_settings('Preferences.sublime-settings')
+
+class Schemr:
+	def load(self):
+		Schemr.scheme = settings.get('color_scheme')
+		Schemr.commands = []
+
+		for root, dirs, files in os.walk(sublime.packages_path()):
+			for filename in files:
+				if filename.endswith('.tmTheme'):
+					Schemr.commands.append({'caption': 'Schemr: ' + os.path.splitext(filename)[0], 'command': 'switch_scheme', 'args': { 's': 'Packages' + '/' + os.path.split(root)[1] + '/' + filename }})
+
+		Schemr.commands.append({'caption': 'Schemr: Reload schemes', 'command': 'reload_schemes'})
+
+		c = open(os.path.join(sublime.packages_path(), 'Schemr', 'Default.sublime-commands'), 'w')
+		c.write(json.dumps(Schemr.commands, indent = 4) + '\n')
+		c.close
+
+Schemr = Schemr()
+sublime.set_timeout(Schemr.load, 3000)
+
+class SwitchSchemeCommand(sublime_plugin.ApplicationCommand):
+	def run(self, s):
+		if self.get_scheme() != s:
+			self.set_scheme(s)
+
+	def get_scheme(self):
+		return settings.get('color_scheme', 'Monokai.tmTheme')
+
+	def set_scheme(self, s):
+		settings.set('color_scheme', s)
+		sublime.save_settings('Preferences.sublime-settings')
+
+class ReloadSchemesCommand(sublime_plugin.ApplicationCommand):
+	def run(self):
+		Schemr.load()
